6ef54a86cf4377af25f7855293e6b2ad28273e56
clement91190 <clem.jambou@gmail.com>
2014-01-17 19:08:50 +0000

    creating app for general user

---
+++ coheal/coheal_user/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+from django.contrib.auth.models import User
+
+class CohealUser(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(null=True, blank=True, upload_to="picture/")
